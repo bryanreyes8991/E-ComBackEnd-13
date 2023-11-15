7076e6b25061c44d64b8e74d39f54273fcaa7f98
Nebula Rider <bryanreyes8991@gmail.com>
2023-11-15 09:40:09 -0600

    built model index

--- models/index.js
+++ models/index.js
@@ -5,13 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsToMany(Category, {
+  foreignKey: "category_id"
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+  otherKey: "product_id",
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: "product_tag",
+  foreignKey: "tag_id",
+  otherKey: "product_id",
+});
 module.exports = {
   Product,
   Category,
